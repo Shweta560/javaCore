f581793ac216339b489e12c35d4a574ce6df6d9c
Shweta560 <shwettaaap98@gmail.com>
2026-09-03 21:59:24 +0530

    Add Java core project

---
+++ src/main/java/EmpSalary.java
@@ -0,0 +1,11 @@
+public class EmpSalary {
+
+    public static void main(String[] args) {
+
+        int salary = 50000;
+        if (salary < 67000) {
+            salary = salary + 5600;
+        }
+        System.out.println(salary);
+    }
+}
